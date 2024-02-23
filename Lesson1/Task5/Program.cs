5c958b707e019e683d090fee20a121dbc4e6b5c5
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 14:57:26 +0800

    add cycle

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int n = 10;
+int i = 1;
+
+while (i <= n)
+{
+    Console.Write(i+ " ");
+    i = i + 1;
+}
