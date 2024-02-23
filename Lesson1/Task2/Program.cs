28cef0192dd02ebfe708ba33025b5cf332468b2c
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 13:06:00 +0800

    add sums

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,3 +1,19 @@
 ﻿int i1 = 5;
-int i2=6;
+int i2 = 6;
 int sum = i1 + i2;
+Console.WriteLine (sum);
+
+
+long l1 = 6;
+long sum2 = l1+i1;
+Console.WriteLine (sum2);
+
+
+double d1 = 5.5;
+double sum3 = d1+i1;
+Console.WriteLine ("sum3");
+
+
+
+
+
