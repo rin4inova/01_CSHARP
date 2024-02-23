680141aa0c300d421a74db17b140473b522afbde
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 16:35:16 +0800

    add array to lesson4: task4

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+{
+    max = array[i];
+}
+i = i + 1;
+}
+
+Console.WriteLine(max);
