523d03b542ede4d8272a13c19e41f04edff00296
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 16:17:18 +0800

    add while to lesson2: task3

--- Lesson2/Task3/Program.cs
+++ Lesson2/Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// int n = 10;
+int[] array = { 1, 5, 4, 7, 8, 35, 15, 3, 8, 7 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    i = i + 1;
+}
+
+
