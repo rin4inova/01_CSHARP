3e4853a6bc00bf554625ab0fb26c35a36a7c3cb1
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 17:01:11 +0800

    add FOR and FOREACH to lesson2 :task4

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -13,3 +13,25 @@ i = i + 1;
 }
 
 Console.WriteLine(max);
+
+max = array[0];
+for (int j =0; j < n; j++)
+{
+    if (array[j] > max)
+{
+    max = array[j];
+}
+j = j + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+
+Console.WriteLine(max);
