878bae14203fcef61eaa265defc5e87d5a00d3b6
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 14:38:41 +0800

    add 5 point

--- Lesson1/Task4/Program.cs
+++ Lesson1/Task4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 6;
+int b = 7;
+int c = 8;
+int d = 7;
+int e = 7;
+
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine ("max = " + max);
