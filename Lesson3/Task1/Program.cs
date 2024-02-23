a93eff80c8a34431adec989d0cb8a8f9dc065c83
Tuyana Rinchinova <rin4inova@gmail.com>
2024-02-23 17:40:47 +0800

    add function

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+
+}
+
+
+double result = CalculateFormula(1, 2, 3, 4);
+
+Console.WriteLine(result);
