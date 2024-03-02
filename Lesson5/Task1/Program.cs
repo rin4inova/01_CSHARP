80220e831301e9afcb1c4b05aa7d78192be3ea5e
Tuyana Rinchinova <rin4inova@gmail.com>
2024-03-02 15:26:45 +0800

    lesson 5 task 1 add

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Factorial(int n)
+{
+    if (n == 1 || n == 0)
+    {
+       
+        return 1;
+    }
+    Console.WriteLine(n);
+   
+    return n * Factorial(n - 1);
+    
+}
+
+Console.WriteLine(Factorial(5));
